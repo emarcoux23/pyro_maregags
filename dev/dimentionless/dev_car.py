58cd87575cd7074336c469507b66843442e4b503
Alexandre Girard <alx87grd@gmail.com>
2023-09-13 22:57:17 -0400

    car analysis

--- dev/dimentionless/dev_car.py
+++ dev/dimentionless/dev_car.py
@@ -15,61 +15,65 @@ from pyro.planning import dynamicprogramming
 from pyro.analysis import graphical
 ##############################################################################
 
-
-m = 1
-g = 1
-l = 1
-t_max_star = 1
-q_star = 1
-case_name = 'test'
+case_name = 'car_test_l5_w50'
 show = True
 res = 'std'
 legend = 1
 
-# Additionnal fixed domain dimentionless parameters
-theta_star  = 2.0 * np.pi
-dtheta_star = 1.0 * np.pi
-time_star   = 2.0 * np.pi * 20.0
 
-# Combined system parameters
-omega = np.sqrt( ( g / l  ) )
-mgl   = m * g * l
+#dim context
 
-# Dimentional parameters
-t_max  = t_max_star * mgl
-q      = q_star * mgl
-theta  = theta_star
-dtheta = dtheta_star * omega
-time   = time_star / omega
-J_max  = mgl**2 / omega * time_star * ( ( q_star * theta_star )**2 + t_max_star**2 )
+x_c_star = 0.5
+y_c_star = 0.5 
+x_w_star = 50.0
+
+# param
+length  = 5.0
+gravity = 9.8
+
+# context
+x_c = x_c_star * length
+y_c = y_c_star * length
+x_w = x_w_star * length
+
+# additionnal domain
+x_max = 10 * length
+v_max = 2 * np.sqrt( gravity * length )
+t_max = 10 * x_max / v_max
+s_max = 0.3
+j_max = t_max * ( ( x_max / x_w  ) **2 + s_max ** 2 )
+
 
 print('\n\nCase :' + case_name )
 print('----------------------------------------------------')
-print(' m=',m,' g=',g,' l=',l,' t_max=', t_max, ' q=', q)
+print(' l=',length,' x_c=', x_c, ' y_c=', y_c,'x_w=', x_w)
 
 ################################
 # Dynamic system definition
 ################################
 
-sys  = longitudinal_vehicule.
+sys  = longitudinal_vehicule.LongitudinalFrontWheelDriveCarWithWheelSlipInput()
 
-# kinematic
-sys.lc1 = l
+# Model param
+sys.lenght  = length          # distance between front wheel and back wheel [m]
+sys.xc      = x_c          # distance from back wheel to c.g.  [m]
+sys.yc      = y_c       # height from ground to c.g.  [m]
 
-sys.l1       = sys.lc1
-sys.l_domain = sys.lc1 * 2
+sys.gravity = gravity       # gravity constant [N/kg]
 
-# dynamic
-sys.m1       = m
-sys.I1       = 0
-sys.gravity  = g
-sys.d1       = 0
+sys.mass    = 1.       # total car mass [kg]
+sys.rho     = 1.      # air density [kg/m3]
+sys.cdA     = 0.    # drag coef time area [m2]
 
-sys.u_ub[0]  = + t_max
-sys.u_lb[0]  = - t_max
+# Ground traction curve parameters
+sys.mu_max   = 0.5
+sys.mu_slope = 70.
 
-sys.x_ub = np.array([ + theta , + dtheta ])
-sys.x_lb = np.array([ - theta , - dtheta ])
+sys.u_ub[0]  = + s_max
+sys.u_lb[0]  = - s_max
+
+sys.x_ub = np.array([ + x_max , + v_max])
+sys.x_lb = np.array([ - x_max , - v_max])
 
 ################################
 # Discritized grid
@@ -97,7 +101,7 @@ else:
     
     dt = 0.05
     nx = 101
-    nu = 11
+    nu = 21
         
 grid_sys = discretizer.GridDynamicSystem( sys , [nx,nx] , [nu] , dt , True )
 
@@ -108,9 +112,11 @@ grid_sys = discretizer.GridDynamicSystem( sys , [nx,nx] , [nu] , dt , True )
 qcf = costfunction.QuadraticCostFunction.from_sys(sys)
 
 qcf.xbar = np.array([ 0 , 0 ]) # target
-qcf.INF  = J_max
 
-qcf.Q[0,0] = q ** 2
+qcf.INF  = j_max
+
+
+qcf.Q[0,0] = (1./x_w) ** 2
 qcf.Q[1,1] = 0.0
 
 qcf.R[0,0] = 1.0
@@ -121,14 +127,16 @@ qcf.R[0,0] = 1.0
 
 dp = dynamicprogramming.DynamicProgrammingWithLookUpTable( grid_sys, qcf )
 
-steps = int( time / dt ) 
+steps = int( t_max / dt ) 
 
 dp.compute_steps( steps )
 
 
 #grid_sys.fontsize = 10
-qcf.INF  = 0.1 * J_max
+# qcf.INF  = 0.1 * qcf.INF
 dp.clean_infeasible_set()
+
+dp.plot_policy()
 
 
 ##################################
@@ -139,25 +147,25 @@ dpi      = 300
 fontsize = 10
 figsize  = (4, 3)
 
-##################################
-# Dimensional policy plot
-##################################
+# ##################################
+# # Dimensional policy plot
+# ##################################
 
 fig = plt.figure( figsize = figsize, dpi=dpi, frameon=True)
 fig.canvas.manager.set_window_title( 'dimentionless policy' )
 ax  = fig.add_subplot(1, 1, 1)
 
-xname = r'$\theta  \; [rad]$'
-yname = r'$\dot{\theta} \; [rad/sec]$'
-zname = r'$\tau \; [Nm]$'
+xname = r'$x  \; [m]$'
+yname = r'$\dot{x} \; [m/sec]$'
+zname = r'$s \; [-]$'
 
-sys.state_label[0] = r'$\theta$'
-sys.state_label[1] = r'$\dot{\theta}$'
-sys.input_label[0] = r'$\tau$'
+sys.state_label[0] = r'$x$'
+sys.state_label[1] = r'$\dot{x}$'
+sys.input_label[0] = r'$s$'
 
-xrange = 2.0 * np.pi
-yrange = np.pi * np.sqrt( 10 / 1. )
-zrange = 20.
+xrange = 50
+yrange = 15
+zrange = 0.3
 
 ax.set_ylabel( yname, fontsize = fontsize )
 ax.set_xlabel( xname, fontsize = fontsize )
@@ -191,41 +199,14 @@ cbar.set_label(zname, fontsize = fontsize , rotation = 90 )
 fig.tight_layout()
 #fig.show()
 fig.savefig( case_name + '_policy.pdf')
-fig.savefig( case_name + '_policy.png')
-fig.savefig( case_name + '_policy.jpg')
+# fig.savefig( case_name + '_policy.png')
+# fig.savefig( case_name + '_policy.jpg')
 
 if show:
     plt.show()
 else:
     plt.close( fig )
-
-
-##################################
-# Trajectory plot
-##################################
-
-ctl = dp.get_lookup_table_controller()
-
-# Simulation
-cl_sys = ctl + sys
-cl_sys.x0   = np.array([-3.14, 0.])
-cl_sys.compute_trajectory( 10 , 6001, 'euler')
-
-tp = graphical.TrajectoryPlotter( sys )
-tp.fontsize = fontsize
-tp.plot( cl_sys.traj , 'xu' , show = False )
-tp.plots[1].set_ylim([-5.5, 5.5])
-tp.plots[2].set_ylim([-zrange, zrange])
-tp.fig.savefig( case_name + '_traj.pdf')
-tp.fig.savefig( case_name + '_traj.png')
-tp.fig.savefig( case_name + '_traj.jpg')
-
-if show:
-    plt.show()
-else:
-    plt.close( tp.fig )
-
-
+    
 ##################################
 # Dimensionless policy plot
 ##################################
@@ -234,20 +215,20 @@ fig = plt.figure( figsize= figsize, dpi=dpi, frameon=True)
 fig.canvas.manager.set_window_title( 'dimentionless policy' )
 ax  = fig.add_subplot(1, 1, 1)
 
-xname = r'$\theta^*$'#self.sys.state_label[x] #+ ' ' + self.sys.state_units[x]
-yname = r'$\dot{\theta}^* = \frac{\dot{\theta}}{\omega}$'#self.sys.state_label[y] #+ ' ' + self.sys.state_units[y]
-zname = r'$\tau^*=\frac{\tau}{mgl}$'
+xname = r'$x^*$'#self.sys.state_label[x] #+ ' ' + self.sys.state_units[x]
+yname = r'$\dot{x}^* = \frac{\dot{x}}{\sqrt{gl}}$'#self.sys.state_label[y] #+ ' ' + self.sys.state_units[y]
+zname = r'$s$'
 
 ax.set_ylabel(yname, fontsize = fontsize )
 ax.set_xlabel(xname, fontsize = fontsize )
 
-x_level = grid_sys.x_level[ 0 ] * 1
-y_level = grid_sys.x_level[ 1 ] * (1 / omega)
+x_level = grid_sys.x_level[ 0 ] * 1./length
+y_level = grid_sys.x_level[ 1 ] * (1 / np.sqrt( gravity * length ))
 
 
 u = grid_sys.get_input_from_policy( dp.pi , 0 )
 
-u2 =  u * (1/mgl)
+u2 =  u 
 
 J_grid_nd = grid_sys.get_grid_from_array( u2 ) 
 
@@ -270,15 +251,38 @@ cbar.set_label(zname, fontsize = fontsize , rotation = 90 )
 fig.tight_layout()
 #fig.show()
 fig.savefig( case_name + '_dimpolicy.pdf')
-fig.savefig( case_name + '_dimpolicy.png')
-fig.savefig( case_name + '_dimpolicy.jpg')
+# fig.savefig( case_name + '_dimpolicy.png')
+# fig.savefig( case_name + '_dimpolicy.jpg')
+
+
+# ##################################
+# # Trajectory plot
+# ##################################
+
+ctl = dp.get_lookup_table_controller()
+
+# Simulation
+cl_sys = ctl + sys
+cl_sys.x0   = np.array([-x_max * 0.5, 0.])
+cl_sys.compute_trajectory( 10 , 6001, 'euler')
+
+tp = graphical.TrajectoryPlotter( sys )
+tp.fontsize = fontsize
+tp.plot( cl_sys.traj , 'xu' , show = False )
+tp.plots[1].set_ylim([-5.5, 5.5])
+tp.plots[2].set_ylim([-zrange, zrange])
+tp.fig.savefig( case_name + '_traj.pdf')
+# tp.fig.savefig( case_name + '_traj.png')
+# tp.fig.savefig( case_name + '_traj.jpg')
 
 if show:
     plt.show()
 else:
-    plt.close( fig )
+    plt.close( tp.fig )
 
-    
+cl_sys.plot_trajectory('xu')
+cl_sys.plot_phase_plane_trajectory()
+
 
     
 
